c8099aa547d6488661613941da90e782c1be5684
a0975660266 <71679957+a0975660266@users.noreply.github.com>
2021-05-11 16:03:13 +0800

    ball

---
+++ AI_p1/Assets/Script/Bomping.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class Bomping : MonoBehaviour
+{
+    public Rigidbody rigid;
+
+    public Vector3 force;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rigid.AddForce(force);
+    }
+}
---
+++ AI_p1/Assets/Script/Rotate.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 xyz;
+    void Update()
+    {
+        Input.GetKey
+    }
+}
